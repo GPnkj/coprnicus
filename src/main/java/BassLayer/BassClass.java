7c3b7d9c08d48c1584d0a1a874f8623f9dfa64b7
BusinessComputers.in <BusinessComputers.in@DESKTOP-BTO5A6T>
2024-01-16 11:44:58 +0530

    copernicus framework created

---
+++ src/main/java/BassLayer/BassClass.java
@@ -0,0 +1,13 @@
+package BassLayer;
+
+
+
+public class BassClass {
+	public void initelization () {
+	
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user+dir")+"\\src\\main\\java\\All Drivers\\chromedriver.exe");
+		
+		
+	
+	}
+}
